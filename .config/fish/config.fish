2f202c0b0c5a86255e3bb928bb5dd440083f9e11
voidashi <jeffmbueno@gmail.com>
2024-10-19 00:55:40 -0300

    Adicionado /home/jeff/.config/fish a .dotfiles e criado symlink

---
+++ .config/fish/config.fish
@@ -0,0 +1,13 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    
+    starship init fish | source
+
+end
+
+function fish_greeting
+
+    #pfetch
+    catnap
+
+end
