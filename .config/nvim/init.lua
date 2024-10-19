cf01d33eece0ced9ed69cd8997cc11e9df45d1f5
voidashi <jeffmbueno@gmail.com>
2024-10-19 00:55:52 -0300

    Adicionado /home/jeff/.config/nvim a .dotfiles e criado symlink

---
+++ .config/nvim/init.lua
@@ -0,0 +1,5 @@
+-- ~/nvim/init.lua
+
+require("voidashi.settings")
+require("voidashi.maps")
+require("voidashi.lazy")
---
+++ .config/nvim/lua/voidashi/plugins/autopairs.lua
@@ -0,0 +1,11 @@
+-- ~/nvim/lua/slydragonn/plugins/autopairs.lua
+
+return {
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    config = function()
+        require("nvim-autopairs").setup({
+            disable_filetype = { "TelescopePrompt", "vim" },
+        })
+    end,
+}
